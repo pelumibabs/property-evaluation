38738ea995e74e2c1bd18c26b85b417f390ac361
Francis Akoji <akfrendo@gmail.com>
2023-08-10 09:49:44 +0100

    update: add prediction endpoint

--- pages/createTicket/index.tsx
+++ pages/createTicket/index.tsx
@@ -1,11 +1,50 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { MainLayout } from "@/components/templates/MainLayout";
+import FormComponent from "@/components/forms/form";
+
 export interface IAppProps {}
 
 export default function App(props: IAppProps) {
+  const [result, setResult] = useState(null);
+  interface FormData {
+    bedrooms: string;
+    bathrooms: string;
+    yr_built: string;
+    floors: string;
+  }
+  const handleFormSubmit = async (formData: FormData) => {
+    try {
+      const response = await fetch("https://property-eval.onrender.com/predict", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (!response.ok) {
+        throw new Error("Request failed");
+      }
+
+      const data = await response.json();
+      setResult(data);
+    } catch (error) {
+      console.error("Error:", error);
+      setResult(null); // Clear the result in case of an error
+    }
+  };
+
   return (
     <MainLayout>
       <div>Create a ticket</div>
+      <section className="p-8">
+        <FormComponent onSubmit={handleFormSubmit} />
+        {result && (
+          <div className="mt-4">
+            Result: <pre>{JSON.stringify(result, null, 2)}</pre>
+          </div>
+        )}
+      </section>
     </MainLayout>
   );
 }
